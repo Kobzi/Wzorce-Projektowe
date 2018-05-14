d9ef82e6f8015c8fb0b8c733ea863afaff0ab681
Łukasz <abece159@gmail.com>
2018-05-14 20:14:37 +0200

    Fasada
    start

---
+++ Fasada/Fasada/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasada
+{
+    class Person
+    {
+        private string imie;
+        private string nazwisko;
+
+        public void setImie(string imie)
+        {
+            this.imie = imie;
+        }
+
+        public void setNazwisko(string nazwisko)
+        {
+            this.nazwisko = nazwisko;
+        }
+
+    }
+}
