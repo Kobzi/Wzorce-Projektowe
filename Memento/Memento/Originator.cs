8fa71306275e6d1f1c28e139c25257dd20414fae
Łukasz <abece159@gmail.com>
2018-05-21 13:37:04 +0200

    Memento

---
+++ Memento/Memento/Originator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class Originator
+    {
+        private string state;
+
+        public Memento CreateMemento()
+        {
+            return new Memento(state);
+        }
+
+        public void SetMemento(Memento memento)
+        {
+            this.state = memento.GetState();
+        }
+        public void SetState(string state)
+        {
+            this.state = state;
+        }
+        public string GetState()
+        {
+            return this.state;
+        }
+    }
+}
---
+++ Memento/Memento/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Caretaker caretaker = new Caretaker();
+            Originator originator = new Originator();
+            string wartosc;
+
+            wartosc = "nazwa1";
+            originator.SetState(wartosc);
+            caretaker.Memento = originator.CreateMemento();
+
+            wartosc = "nazwa2";
+            originator.SetState(wartosc);
+            caretaker.Memento = originator.CreateMemento();
+
+            wartosc = "nazwa3";
+            wartosc = "nazwa4";
+
+            Console.WriteLine("Ostatnia wartość: " + wartosc);
+
+            // przywracanie poprzedniej wartosci do originatora a potem do zmiennej
+            originator.SetMemento(caretaker.Memento); 
+            wartosc = originator.GetState();
+            Console.WriteLine("Przywrocona wartosc: " +wartosc);
+
+            Console.ReadLine();
+        }
+    }
+}
